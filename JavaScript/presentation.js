da49a2e502b5e8eb21ed72a715a6b618bd45e6a8
thelalakei <thelalakei@gmail.com>
2020-10-29 02:49:45 +0200

    Create presentation.js

---
+++ JavaScript/presentation.js
@@ -0,0 +1,12 @@
+const video = document.getElementById("myVid");
+const btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
